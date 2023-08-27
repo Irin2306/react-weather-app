609100e22a4181f4c2c5cebeeaad0732c419d6df
Irin2306 <135467939+Irin2306@users.noreply.github.com>
2023-08-28 01:59:06 +0300

    Added props

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="New York"/>
         <footer>
           This project is coded by Iryna Podust and is
           <a href="https://github.com/Irin2306/react-weather-app" target="_blank" rel="noreferrer"> open-sourced on GitHub</a>
--- src/Weather.js
+++ src/Weather.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
+export default function Weather(props) {
     const [weatherData, setWeatherData] = useState({ ready: false });
     function handleResponse(response) {
         setWeatherData({
@@ -59,9 +59,10 @@ export default function Weather() {
         );
     } else {
         const apiKey = "5da7b2dc058f07286fea39c4cee516a3";
-        let city = "London";
-        let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+        let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
         axios.get(apiUrl).then(handleResponse);
+
+        return "Loading...";
     }
 }
 
