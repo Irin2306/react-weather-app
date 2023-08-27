3a4fbd2eadcb06d57faa1650888731ba77ae3715
Irin2306 <135467939+Irin2306@users.noreply.github.com>
2023-08-27 21:57:02 +0300

    Added html structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import './App.css';
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        Weather app
+        <Weather />
         <footer>
           This project is coded by Iryna Podust and is
           <a href="https://github.com/Irin2306/react-weather-app" target="_blank" rel="noreferrer"> open-sourced on GitHub</a>
