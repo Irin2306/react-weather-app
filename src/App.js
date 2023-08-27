c82f9ce92e753acba161d6c69b6209d5b842930f
Irin2306 <135467939+Irin2306@users.noreply.github.com>
2023-08-27 21:17:05 +0300

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather app
-      <footer>
-        This project is coded by Iryna Podust and is
-        <a href="https://github.com/Irin2306/react-weather-app" target="_blank" rel="noreferrer"> open-sourced on GitHub</a>
-      </footer>
+      <div className="container">
+        Weather app
+        <footer>
+          This project is coded by Iryna Podust and is
+          <a href="https://github.com/Irin2306/react-weather-app" target="_blank" rel="noreferrer"> open-sourced on GitHub</a>
+        </footer>
+      </div>
     </div>
   );
 }
